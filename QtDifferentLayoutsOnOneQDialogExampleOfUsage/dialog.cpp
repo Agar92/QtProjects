e3dc8bfa784f62485f7fe1282c26c40def3231b3
Andrey <agar10@yandex.ru>
2020-05-21 19:53:50 +0300

    Added QtDifferentLayoutsOnOneQDialogExampleOfUsage - the project illustrating how to create flexible GUI interfaces not gtom Qt Designer, but straight from the source code.

---
+++ QtDifferentLayoutsOnOneQDialogExampleOfUsage/dialog.cpp
@@ -0,0 +1,95 @@
+#include <QtWidgets>
+#include "dialog.h"
+
+Dialog::Dialog()
+{
+    createMenu();
+    createHorizontalGroupBox();
+    createGridGroupBox();
+    createFormGroupBox();
+
+    bigEditor = new QTextEdit;
+    bigEditor->setPlainText(tr("This widget takes up all the remaining space "
+                               "in the layout."));
+
+    buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok
+                                     | QDialogButtonBox::Cancel);
+
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->setMenuBar(menuBar);
+    mainLayout->addWidget(horizontalGroupBox);
+    mainLayout->addWidget(gridGroupBox);
+    mainLayout->addWidget(formGroupBox);
+    mainLayout->addWidget(bigEditor);
+    mainLayout->addWidget(buttonBox);
+    setLayout(mainLayout);
+    setWindowTitle(tr("Basic Layouts"));
+}
+
+// See here: https://stackoverflow.com/questions/26791102/qt-use-of-in-tr
+
+//As far as tr is concerned & is just another character in the string
+//and has no special meaning. However it does have a special meaning
+//to QAction: It sets the following character as the entry's shortcut
+//within a menu, so when you have the "File" menu open, pressing the n
+//key will activate the "New" entry, x will active "Exit" and so on.
+//Depending on the OS, the character after the & may also be underlined
+//(on Windows it's only underlined when you press the Alt key).
+
+//& is used to indicate a character that should be used for the
+//Alt-key shortcut for a given menu item. Assuming these actions
+//are being put into a menu named &File, pressing Alt-F, then N
+//would activate the New action.
+
+void Dialog::createMenu()
+{
+    menuBar = new QMenuBar;
+    fileMenu = new QMenu(tr("&File"), this);
+    //PRESS Alt + F AND THEN x will lead to an exit:
+    exitAction = fileMenu->addAction(tr("E&xit"));
+    menuBar->addMenu(fileMenu);
+    connect(exitAction, &QAction::triggered, this, &QDialog::accept);
+}
+
+void Dialog::createHorizontalGroupBox()
+{
+    horizontalGroupBox = new QGroupBox(tr("Horizontal layout"));
+    QHBoxLayout *layout = new QHBoxLayout;
+    for (int i = 0; i < NumButtons; ++i) {
+        buttons[i] = new QPushButton(tr("Button %1").arg(i + 1));
+        layout->addWidget(buttons[i]);
+    }
+    horizontalGroupBox->setLayout(layout);
+}
+
+void Dialog::createGridGroupBox()
+{
+    gridGroupBox = new QGroupBox(tr("Grid layout"));
+    QGridLayout *layout = new QGridLayout;
+    for (int i = 0; i < NumGridRows; ++i) {
+        labels[i] = new QLabel(tr("Line %1:").arg(i + 1));
+        lineEdits[i] = new QLineEdit;
+        layout->addWidget(labels[i], i + 1, 0);
+        layout->addWidget(lineEdits[i], i + 1, 1);
+    }
+    smallEditor = new QTextEdit;
+    smallEditor->setPlainText(tr("This widget takes up about two thirds of the "
+                                 "grid layout."));
+    layout->addWidget(smallEditor, 0, 2, 4, 1);
+    layout->setColumnStretch(1, 10);
+    layout->setColumnStretch(2, 20);
+    gridGroupBox->setLayout(layout);
+}
+
+void Dialog::createFormGroupBox()
+{
+    formGroupBox = new QGroupBox(tr("Form layout"));
+    QFormLayout *layout = new QFormLayout;
+    layout->addRow(new QLabel(tr("Line 1:")), new QLineEdit);
+    layout->addRow(new QLabel(tr("Line 2, long text:")), new QComboBox);
+    layout->addRow(new QLabel(tr("Line 3:")), new QSpinBox);
+    formGroupBox->setLayout(layout);
+}
+
